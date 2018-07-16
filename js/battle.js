f0c8cf947231e85226905fdccd8580ab603ccd85
dstrauss <fmdenver@gmail.com>
2018-07-15 23:45:42 -0400

    fixed a few things

--- js/battle.js
+++ js/battle.js
@@ -1,31 +1,39 @@
 attackButton.addEventListener("click",function(){
-attackRoll(); 
+    attackRoll(); 
 })
 
 runButton.addEventListener("click",function(){
-runNow();
+    runNow();
+})
+
+grenadeButton.addEventListener("click",function(){
+    window.location = "victory.html";
 })
 
 var attackRoll = function attackRoll(){
-var playMinus = getRandomNumber(0,6),
-monstMinus = getRandomNumber(0,6);
-sword.play();
-PlayHP = PlayHP - playMinus; 
-playerHP.innerHTML = "Player HP <br>" + "<div class='hp-number'>" + PlayHP + "</div>";
-MonstHP = MonstHP - monstMinus;
-monsterHP.innerHTML= "Rabbit HP <br>" + "<div class='hp-number'>" + MonstHP + "</div>";
-var minusValue = monstMinus,
-minusPlayer = playMinus;
-text.innerHTML = "You hit the Rabbit for " + minusValue + "<br> The Rabbit Hits you for " + minusPlayer;
-if (PlayHP <= 0) {
-window.location = "murder.html";
-}
-if (MonstHP <= 0 && PlayHP > 0) {
-window.location = "victory.html";
-}
+    var playMinus = getRandomNumber(0,6),
+    monstMinus = getRandomNumber(0,6);
+    sword.play();
+    PlayHP = PlayHP - playMinus; 
+    MonstHP = MonstHP - monstMinus;
+    dispHP();
+    text.innerHTML = "You hit the Rabbit for " + monstMinus + "<br> The Rabbit Hits you for " + playMinus;
+
+    if (PlayHP <= 0 && MonstHP > 0 ) {
+        window.location = "murder.html";
+    }
+    if (MonstHP <= 0 && PlayHP > 0) {
+        window.location = "victory.html";
+    }
+    if (MonstHP <=6 && PlayHP <=6 ){
+        text.innerHTML = storyItems[3];
+        grenadeButton.classList.toggle("toggleon");
+        attackButton.classList.remove("toggleon");
+        runButton.classList.remove("toggleon");
+    }
 }
 
 var runNow = function runNow(){
-var randy = getRandomNumber(0,1);
-window.location = runpages[randy];
+    var randy = getRandomNumber(0,1);
+    window.location = runpages[randy];
 }
--- js/js1.js
+++ js/js1.js
@@ -3,15 +3,17 @@ var playerHP = document.getElementById("player-hp"),
     text = document.getElementById("story-box"),
     storyImage = document.getElementById("story-image"),
     attackButton = document.getElementById("attack-button"),
-    runButton = document.getElementById("run-button")
+    runButton = document.getElementById("run-button"),
+    grenadeButton =document.getElementById("grenade-button"),
     hpBox = document.getElementById("hp-box"),
     monsterHit = document.getElementById("monster-hit"),
     playerHit = document.getElementById("player-hit");
 
 var storyItems = [
-"<p>You Enter a Dark Cave</p>",
-"<p>As you venture deeper, you hear the pitter-patter of ominous footsteps</p>",
-"<p>It's a nasty little rabbit with BIIIIG Teeth</p>" ]; 
+    "<p>You Enter a Dark Cave</p>",
+    "<p>As you venture deeper, you hear the pitter-patter of ominous footsteps</p>",
+    "<p>It's a nasty little rabbit with BIIIIG Teeth</p>",
+    "<p>You have fought the Beast to a stand still, there is not hope for victory, unless...</p>"  ]; 
 
 var illustration = [
     "<img src='img/img1.jpg'>",
@@ -20,32 +22,43 @@ var illustration = [
 ];
 
 var cave = new Audio('mp3/cave.mp3'),
-footsteps = new Audio('mp3/footsteps.mp3'),
-rabbit = new Audio('mp3/rabbit.mp3')
-sword = new Audio('mp3/sword.mp3');
+    footsteps = new Audio('mp3/footsteps.mp3'),
+    rabbit = new Audio('mp3/rabbit.mp3')
+    sword = new Audio('mp3/sword.mp3');
 
 
 var PlayHP = 20,
-MonstHP = 20;
-runpages=["run1.html","run2.html"];
+    MonstHP = 20;
+    runpages=["run1.html","run2.html"];
 
-setTimeout(function(){
+ setTimeout(load1, 500);
+ setTimeout(load2, 3500);
+ setTimeout(load3, 6500);
+
+
+var dispHP =  function dispHP(){
+    playerHP.innerHTML = "Player HP <br>" + "<div class='hp-number'>" + PlayHP + "</div>";
+    monsterHP.innerHTML= "Rabbit HP <br>" + "<div class='hp-number'>" + MonstHP + "</div>";
+}
+
+function load1(){
     cave.play();
     text.innerHTML = storyItems[0];
-    storyImage.innerHTML = illustration[0]}, 500);
+    storyImage.innerHTML = illustration[0];
+}
 
-setTimeout(function(){
+function load2(){
     footsteps.play();
     text.innerHTML = storyItems[1];
-    storyImage.innerHTML = illustration[1]}, 3500);
+    storyImage.innerHTML = illustration[1];
+}
 
-setTimeout(function(){
+function load3(){
     rabbit.play();
     text.innerHTML = storyItems[2];
     storyImage.innerHTML = illustration[2];
-    playerHP.innerHTML = "Player HP <br>" + "<div class='hp-number'>" + PlayHP + "</div>";
-    monsterHP.innerHTML= "Rabbit HP <br>" + "<div class='hp-number'>" + MonstHP + "</div>";
     attackButton.classList.toggle("toggleon");
     runButton.classList.toggle("toggleon");
-    hpBox.classList.toggle("toggleon")}, 6500); 
-
+    hpBox.classList.toggle("toggleon");
+    dispHP();
+}
